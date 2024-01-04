8594a4043f7d76bd97c17bf73724656e0a65ab15
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-04 11:12:52 +0000

    feat graphics : gl graphics
    this comit should include the gl graphics for use in the engine

--- PlunderDogsInSace/Graphics/Graphics.cpp
+++ PlunderDogsInSace/Graphics/Graphics.cpp
@@ -1,4 +1,5 @@
 #include "Graphics.h"
+#include "GLFW/glfw3.h"
 
 Graphics::Graphics() : m_screenWidth(1280.f), m_screenHeight(720.f)
 {
@@ -6,4 +7,49 @@ Graphics::Graphics() : m_screenWidth(1280.f), m_screenHeight(720.f)
 
 Graphics::Graphics(float ScreenWidth, float ScreenHeight) : m_screenWidth(ScreenWidth), m_screenHeight(m_screenHeight)
 {
+}
+
+GLGraphics::GLGraphics() : Graphics()
+{
+}
+
+GLGraphics::GLGraphics(float ScreenWidth, float ScreenHeight) : Graphics(ScreenWidth, ScreenHeight)
+{
+}
+
+GLGraphics::~GLGraphics()
+{
+	glfwDestroyWindow(m_window);
+
+	glfwTerminate();
+}
+
+void GLGraphics::Init()
+{
+	glfwInit();
+
+	glfwCreateWindow(m_screenWidth, m_screenHeight, "testing", nullptr, nullptr);
+}
+
+void GLGraphics::SetCallbacks()
+{
+}
+
+void GLGraphics::ResizeScreen()
+{
+}
+
+bool GLGraphics::ShouldWindowClose()
+{
+	return glfwWindowShouldClose(m_window);
+}
+
+void GLGraphics::Clear()
+{
+	glClearColor(0.1f, 0.5f, 0.1f, 1.f);
+}
+
+void GLGraphics::Display()
+{
+	glfwSwapBuffers(m_window);
 }
--- PlunderDogsInSace/Graphics/Graphics.h
+++ PlunderDogsInSace/Graphics/Graphics.h
@@ -8,9 +8,26 @@ public:
 	virtual void Init() = 0;
 	virtual void SetCallbacks() = 0;
 	virtual void ResizeScreen() = 0;
+	virtual bool ShouldWindowClose() = 0;
 	virtual void Clear() = 0;
 	virtual void Display() = 0;
 protected:
 	float m_screenWidth, m_screenHeight;
 };
 
+class GLGraphics : public Graphics
+{
+public:
+	GLGraphics();
+	GLGraphics(float ScreenWidth, float ScreenHeight);
+	~GLGraphics();
+public:
+	virtual void Init() override;
+	virtual void SetCallbacks() override;
+	virtual void ResizeScreen() override;
+	virtual bool ShouldWindowClose() override;
+	virtual void Clear() override;
+	virtual void Display() override;
+public:
+	GLFWwindow* m_window;
+};
