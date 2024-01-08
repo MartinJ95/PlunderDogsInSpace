997ce89223b86dbc9ddd456a4922ac1a299f0b2c
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-08 04:36:36 +0000

    feat aplication : flow improvement
    this commit should allow the engine to work multiple steps and manage delta time.

--- PlunderDogsInSpace/Core/Application.cpp
+++ PlunderDogsInSpace/Core/Application.cpp
@@ -9,12 +9,32 @@ bool Application::Init()
     return m_graphics.Init();
 }
 
+void Application::Render()
+{
+}
+
+void Application::PhysicsStep()
+{
+}
+
+void Application::Update()
+{
+}
+
+void Application::EndOfFrame()
+{
+}
+
 void Application::Run()
 {
     while (!m_graphics.ShouldWindowClose())
     {
         m_graphics.Clear();
+        Update();
+        Render();
+        PhysicsStep();
         m_graphics.Display();
         m_graphics.PollEvents();
+        EndOfFrame();
     }
 }
--- PlunderDogsInSpace/Core/Application.h
+++ PlunderDogsInSpace/Core/Application.h
@@ -1,18 +1,38 @@
 #pragma once
 #include "../Graphics/Graphics.h"
+#include <chrono>
 #ifdef OpenGL
 typedef GLGraphics DefaultGraphics;
 #else
 typedef NullGraphics DefaultGraphics;
 #endif // !OpenGL
 
+struct TimeManager
+{
+	TimeManager() : lastUpdate(std::chrono::steady_clock::now()), deltaTime(0.f)
+	{}
+	void TimeStep()
+	{
+		auto now = std::chrono::steady_clock::now();
+		deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(now - lastUpdate).count() / 1000000.f;
+		lastUpdate = now;
+	}
+	std::chrono::steady_clock::time_point lastUpdate;
+	float deltaTime;
+};
+
 class Application
 {
 public:
 	Application();
 	virtual bool Init();
-	virtual void Run();
+	virtual void Render();
+	virtual void PhysicsStep();
+	virtual void Update();
+	virtual void EndOfFrame();
+	void Run();
 protected:
 	DefaultGraphics m_graphics;
+	TimeManager m_timeManager;
 };
 
