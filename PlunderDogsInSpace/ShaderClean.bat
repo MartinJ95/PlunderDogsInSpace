1d6e588fe89710d4eb749971b6b8cd0682d85c61
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-12 10:59:15 +0000

    feat shader : distribution bats
    this commit should allow the solution to include bat files for handling the shader files

---
+++ PlunderDogsInSpace/ShaderClean.bat
@@ -0,0 +1,9 @@
+del ".\x64\Debug\default2DShader.vs"
+del ".\x64\Debug\default2DShader.fs"
+del ".\x64\Debug\default3DShader.vs"
+del ".\x64\Debug\default3DShader.fs"
+
+del ".\Game\default2DShader.vs"
+del ".\Game\default2DShader.fs"
+del ".\Game\default3DShader.vs"
+del ".\Game\default3DShader.fs"
---
+++ PlunderDogsInSpace/ShaderDistribute.bat
@@ -0,0 +1,9 @@
+robocopy ".\Graphics" ".\Game" default2DShader.vs
+robocopy ".\Graphics" ".\Game" default2DShader.fs
+robocopy ".\Graphics" ".\Game" default3DShader.vs
+robocopy ".\Graphics" ".\Game" default3DShader.fs
+
+robocopy ".\Graphics" ".\x64\Debug" default2DShader.vs
+robocopy ".\Graphics" ".\x64\Debug" default2DShader.fs
+robocopy ".\Graphics" ".\x64\Debug" default3DShader.vs
+robocopy ".\Graphics" ".\x64\Debug" default3DShader.fs
