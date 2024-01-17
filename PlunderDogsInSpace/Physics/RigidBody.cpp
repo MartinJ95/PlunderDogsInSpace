155f109f292e965b25a031b3978dc19dc54ae0a8
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-17 15:15:03 +0000

    faet physics : application of physics
    this commit should provide the full initial impleentatiopn of the rigid body class

--- PlunderDogsInSpace/Physics/RigidBody.cpp
+++ PlunderDogsInSpace/Physics/RigidBody.cpp
@@ -1,5 +1,48 @@
 #include "RigidBody.h"
 
-RigidBody::RigidBody() : m_position(glm::vec3(0.f)), m_velocity(glm::vec3(0.f)), m_force(glm::vec3(0.f)), m_constantForce(glm::vec3(0.f, 0.f, 0.f))
+RigidBody::RigidBody() : m_position(glm::vec3(0.f)), m_velocity(glm::vec3(0.f)), m_force(glm::vec3(0.f)), m_constantForce(glm::vec3(0.f, 0.f, 0.f)), m_mass(10.f)
 {
+}
+
+void RigidBody::ApplyPhysics(const float DeltaTime, const glm::quat& Rotation)
+{
+	glm::vec3 VelocityChange = GetVelocityChange(DeltaTime);
+	ApplyRotation(VelocityChange, Rotation);
+
+	m_velocity += VelocityChange;
+
+	m_velocity *= 0.9f;
+
+	m_position += m_velocity;
+}
+
+const glm::vec3& RigidBody::GetPosition() const
+{
+	return m_position;
+}
+
+void RigidBody::SetForce(const glm::vec3& NewForce)
+{
+	m_force = NewForce;
+}
+
+void RigidBody::AddForce(const glm::vec3& AddedForce)
+{
+	m_force += AddedForce;
+}
+
+void RigidBody::SubtractForce(const glm::vec3& SubtractedForce)
+{
+	m_force -= SubtractedForce;
+}
+
+inline glm::vec3 RigidBody::GetVelocityChange(const float DeltaTime) const
+{
+	glm::vec3 acceleration = (m_force + m_constantForce) / m_mass;
+	return acceleration * DeltaTime;
+}
+
+inline glm::vec3 RigidBody::ApplyRotation(const glm::vec3& VelocityChange, const glm::quat& Rotation)
+{
+	return Rotation * VelocityChange * glm::conjugate(Rotation);
 }
--- PlunderDogsInSpace/Physics/RigidBody.h
+++ PlunderDogsInSpace/Physics/RigidBody.h
@@ -1,18 +1,24 @@
 #pragma once
 #include "../Dependencies/Include/glm/glm/glm.hpp"
+#include "../Dependencies/Include/glm/glm/gtc/quaternion.hpp"
 
 class RigidBody
 {
 public:
 	RigidBody();
-	void ApplyPhysics(const float DeltaTime);
-	glm::vec3 GetPosition() const;
+	void ApplyPhysics(const float DeltaTime, const glm::quat& Rotation);
+	const glm::vec3& GetPosition() const;
 	void SetForce(const glm::vec3& NewForce);
 	void AddForce(const glm::vec3& AddedForce);
-public:
+	void SubtractForce(const glm::vec3& SubtractedForce);
+private:
+	inline glm::vec3 GetVelocityChange(const float DeltaTime) const;
+	inline glm::vec3 ApplyRotation(const glm::vec3& VelocityChange, const glm::quat& Rotation);
+private:
 	glm::vec3 m_position;
 	glm::vec3 m_velocity;
 	glm::vec3 m_force;
 	glm::vec3 m_constantForce;
+	float m_mass;
 };
 
