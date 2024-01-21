0d74460ccad154b780e2fa038d2b7a1c2326aa5e
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-21 13:25:18 +0000

    feat ray to plane : better collision

--- PlunderDogsInSpace/Physics/Collisions.cpp
+++ PlunderDogsInSpace/Physics/Collisions.cpp
@@ -1,29 +1,2 @@
 #include "Collisions.h"
-#include "../Physics/RigidBody.h"
 
-void RayToPlane(const Ray& Raycast, const PlaneCollider& Plane, CollisionData& Data)
-{
-	glm::vec3 PlanePos = glm::vec3(0.f);
-	if (Data.other != nullptr)
-	{
-		PlanePos = Data.other->GetTransform().position;
-	}
-
-	float originDot = glm::dot(Plane.planeNormal, Raycast.origin - PlanePos);
-	float destDot = glm::dot(Plane.planeNormal, (Raycast.origin + Raycast.line) - PlanePos);
-
-	if ((originDot < 0.f && destDot > 0.f) || (originDot > 0.f && destDot < 0.f))
-	{
-		Data.hasHit = true;
-		Data.collisionNormal = Plane.planeNormal;
-
-		glm::vec3 DestToPlaneOrigin = glm::normalize(PlanePos - (Raycast.origin + Raycast.line));
-		glm::vec3 DestToOrigin = glm::normalize(Raycast.origin - (Raycast.origin + Raycast.line));
-
-		float percentage = glm::dot(DestToPlaneOrigin, DestToOrigin);
-
-		Data.penetrationDepth =  glm::length(PlanePos - (Raycast.origin + Raycast.line)) * percentage;
-
-		Data.pointOfCollision = (Raycast.origin + (Raycast.line * (glm::length(Raycast.line) - Data.penetrationDepth)));
-	}
-}
--- PlunderDogsInSpace/Physics/Collisions.h
+++ PlunderDogsInSpace/Physics/Collisions.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Dependencies/Include/glm/glm/glm.hpp"
+#include "../Physics/RigidBody.h"
 
 class RigidBody;
 
@@ -37,5 +38,30 @@ struct PlaneCollider
 
 
 //raycast functions
-static void RayToPlane(const Ray& Raycast, const PlaneCollider& Plane, CollisionData& Data);
+static void RayToPlane(const Ray& Raycast, const PlaneCollider& Plane, CollisionData& Data)
+{
+	glm::vec3 PlanePos = glm::vec3(0.f);
+	if (Data.other != nullptr)
+	{
+		PlanePos = Data.other->GetTransform().GetPosition();
+	}
+
+	float originDot = glm::dot(Plane.planeNormal, Raycast.origin - PlanePos);
+	float destDot = glm::dot(Plane.planeNormal, (Raycast.origin + Raycast.line) - PlanePos);
+
+	if ((originDot < 0.f && destDot > 0.f) || (originDot > 0.f && destDot < 0.f))
+	{
+		Data.hasHit = true;
+		Data.collisionNormal = Plane.planeNormal;
+
+		glm::vec3 DestToPlaneOrigin = glm::normalize(PlanePos - (Raycast.origin + Raycast.line));
+		glm::vec3 DestToOrigin = glm::normalize(Raycast.origin - (Raycast.origin + Raycast.line));
+
+		float percentage = glm::dot(DestToPlaneOrigin, DestToOrigin);
+
+		Data.penetrationDepth = glm::length(PlanePos - (Raycast.origin + Raycast.line)) * percentage;
+
+		Data.pointOfCollision = (Raycast.origin + (glm::normalize(Raycast.line) * (glm::length(Raycast.line) - Data.penetrationDepth)));
+	}
+}
 static void RayToSphere(const Ray& Raycast, const SphereCollider& Sphere, CollisionData& Data);
