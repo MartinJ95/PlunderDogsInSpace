1b1aa3a65dc41c75b5ab8fb5b0f17b7d807cc13e
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-24 02:53:10 +0000

    feat sandbox : player controller abstraction
    this commit should abstract the player controlling code as it is now in the player controller class

--- PlunderDogsInSpace/Game/Sandbox.cpp
+++ PlunderDogsInSpace/Game/Sandbox.cpp
@@ -12,39 +12,7 @@ bool Sandbox::Init()
 
 void Sandbox::Update()
 {
-    glm::vec3 mouseMovement = glm::vec3(0.f);
-
-    mouseMovement.x -= glfwGetKey(m_graphics.GetWindow(), GLFW_KEY_A);
-    mouseMovement.x += glfwGetKey(m_graphics.GetWindow(), GLFW_KEY_D);
-    mouseMovement.z += glfwGetKey(m_graphics.GetWindow(), GLFW_KEY_S);
-    mouseMovement.z -= glfwGetKey(m_graphics.GetWindow(), GLFW_KEY_W);
-
-    m_graphics.GetCamera().SetPos(m_graphics.GetCamera().GetPos() + (mouseMovement * m_timeManager.deltaTime));
-
-    if (glfwGetMouseButton(m_graphics.GetWindow(), GLFW_MOUSE_BUTTON_1))
-    {
-        Ray ray;
-        ray.origin = m_graphics.GetCamera().GetPos();
-        //ray.line = glm::normalize(m_graphics.GetCamera().GetViewDir()) * 10.f;
-        ray.line = glm::normalize(m_graphics.GetCamera().GetViewDirOnScreen(m_graphics.GetScreenDimensions(), m_graphics.GetMouseLocation())) * 100.f;
-        //std::cout << "x: " << ray.line.x << "\n" << "y: " << ray.line.y << "\n" << "z: " << ray.line.z << "\n" << "\n";
-        //std::cout << "x: " << ray.origin.x << "\n" << "y: " << ray.origin.y << "\n" << "z: " << ray.origin.z << "\n" << "\n";
-
-        PlaneCollider plane;
-        plane.planeNormal = glm::vec3(0.f, 1.f, 0.f);
-
-        CollisionData data;
-
-        RayToPlane(ray, plane, data);
-
-        if (data.hasHit)
-        {
-            m_clickIndicators.emplace_back();
-            m_clickIndicators.back().m_transform.SetPosition(data.pointOfCollision);
-            //m_entities.back().m_body.GetTransform().SetPosition(ray.origin + (ray.line * 0.1f));
-        }
-        //std::cout << data;
-    }
+    m_controller.Update();
 	Application::Update();
 }
 
@@ -57,51 +25,12 @@ void Sandbox::Render()
         0.f, 0.f, 10.f, 0.f,
         0.f, 0.f, 0.f, 10.f
     ));
-
-    m_graphics.GetShader(1).SetRender3D(m_graphics.GetCamera());
-
-    for (int i = 0; i < m_clickIndicators.size(); i++)
-    {
-        m_clickIndicators[i].m_currentTime += m_timeManager.deltaTime;
-        if (m_clickIndicators[i].m_currentTime > m_clickIndicators[i].m_lifetime)
-        {
-            m_clickIndicators[i].m_markedForDeletion = true;
-            continue;
-        }
-        m_graphics.GetShader(1).m_uniforms.SetFloat(m_graphics.GetShader(1).ID, "currentTime", m_clickIndicators[i].m_currentTime);
-        m_graphics.GetShader(1).m_uniforms.SetFloat(m_graphics.GetShader(1).ID, "totalTime", m_clickIndicators[i].m_lifetime);
-        //m_graphics.GetShader(1).m_uniforms.SetVec3(m_graphics.GetShader(1).ID, "cameraPosition", m_graphics.GetCamera().GetPos());
-        m_graphics.Render(0, 1, true, m_clickIndicators[i].m_transform.GetModelXform());
-    }
+    m_controller.Render();
 	Application::Render();
 }
 
 void Sandbox::EndOfFrame()
 {
-    int deletedIndicators = 0;
-
-    for (int i = 0; i < m_clickIndicators.size(); i++)
-    {
-        if (m_clickIndicators[i].m_markedForDeletion)
-        {
-            if (m_clickIndicators.size() > 1)
-            {
-                std::swap(m_clickIndicators[i], m_clickIndicators[m_clickIndicators.size() - ++deletedIndicators]);
-            }
-            else
-            {
-                deletedIndicators++;
-            }
-        }
-    }
-    int initialSize = m_clickIndicators.size();
-    for (int i = 0; i < deletedIndicators; i++)
-    {
-        m_clickIndicators.erase(m_clickIndicators.begin() + (initialSize - (i + 1)));
-    }
-    for (int i = 0; i < m_clickIndicators.size(); i++)
-    {
-        m_clickIndicators[i].m_transform.EndFrame();
-    }
+    m_controller.EndOfFrame();
 	Application::EndOfFrame();
 }
--- PlunderDogsInSpace/Game/Sandbox.h
+++ PlunderDogsInSpace/Game/Sandbox.h
@@ -1,16 +1,6 @@
 #pragma once
 #include "../Core/Application.h"
-
-struct ClickIndicator
-{
-	ClickIndicator() : m_transform(), m_meshID(0), m_currentTime(0.f), m_lifetime(0.5f), m_markedForDeletion(false)
-	{}
-	Transform m_transform;
-	unsigned int m_meshID;
-	float m_currentTime;
-	float m_lifetime;
-	bool m_markedForDeletion;
-};
+#include "PlayerController.h"
 
 class Sandbox : public Application
 {
@@ -21,6 +11,6 @@ public:
 	virtual void Render() override final;
 	virtual void EndOfFrame() override final;
 protected:
-	std::vector<ClickIndicator> m_clickIndicators;
+	PlayerController m_controller;
 };
 
