9148548ad1b7e0678ab937e72be150b6e1a7f6f4
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-24 17:24:05 +0000

    feta game : team construction
    this commit should provde more initial implementation for the team container

--- PlunderDogsInSpace/Game/Team.h
+++ PlunderDogsInSpace/Game/Team.h
@@ -1,11 +1,22 @@
 #pragma once
+#include "../Core/ServiceLocator.h"
+#include "../AI/BehaviourTree.h"
 
 struct ship
 {
-
+	unsigned int ModelID;
+	Transform m_transform;
+	RigidBody m_body;
+	BehaviourTree m_tree;
+	void Update();
+	void Render();
+	void EndOfFrame();
 };
 
 class Team
 {
+	void Update();
+	void Render();
+	void EndOfFrame();
 };
 
