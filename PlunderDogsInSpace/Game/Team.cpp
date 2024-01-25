2b78f5945e78cb2023693ede6e98a771c00c6469
MartinJ95 <martinjones95@gmx.co.uk>
2024-01-25 00:04:09 +0000

    feat team : various additions
    this commit should provide variables for the teams and ships to work properly

--- PlunderDogsInSpace/Game/Team.cpp
+++ PlunderDogsInSpace/Game/Team.cpp
@@ -1,1 +1,60 @@
 #include "Team.h"
+
+Ship::Ship() : ModelID(1), m_transform(), m_body(), m_tree(std::move(new BTSelectorNode))
+{
+}
+
+void Ship::Update()
+{
+	m_tree.Evaluate();
+
+	m_body.ApplyPhysics(ServiceLocator::GetTimeService().deltaTime, m_transform);
+	m_transform.CheckModelXForm();
+}
+
+void Ship::Render()
+{
+	DefaultGraphics& graphics = ServiceLocator::GetGraphics();
+
+	graphics.GetShader(0).SetRender3D(graphics.GetCamera());
+
+	graphics.Render(ModelID, 0, true, m_transform.GetModelXform());
+}
+
+void Ship::EndOfFrame()
+{
+	m_transform.EndFrame();
+}
+
+Team::Team(glm::vec3 StartPos) : m_startPosition(StartPos), m_otherTeamRef(nullptr), m_ships()
+{
+}
+
+void Team::Init(const Team* Other)
+{
+
+}
+
+void Team::Update()
+{
+	for (Ship& s : m_ships)
+	{
+		s.Update();
+	}
+}
+
+void Team::Render()
+{
+	for (Ship& s : m_ships)
+	{
+		s.Update();
+	}
+}
+
+void Team::EndOfFrame()
+{
+	for (Ship& s : m_ships)
+	{
+		s.EndOfFrame();
+	}
+}
--- PlunderDogsInSpace/Game/Team.h
+++ PlunderDogsInSpace/Game/Team.h
@@ -2,8 +2,9 @@
 #include "../Core/ServiceLocator.h"
 #include "../AI/BehaviourTree.h"
 
-struct ship
+struct Ship
 {
+	Ship();
 	unsigned int ModelID;
 	Transform m_transform;
 	RigidBody m_body;
@@ -15,8 +16,16 @@ struct ship
 
 class Team
 {
+public:
+	Team(glm::vec3 StartPos);
+public:
+	void Init(const Team* Other);
 	void Update();
 	void Render();
 	void EndOfFrame();
+protected:
+	glm::vec3 m_startPosition;
+	Team* m_otherTeamRef;
+	std::vector<Ship> m_ships;
 };
 
