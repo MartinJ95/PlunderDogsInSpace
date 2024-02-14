123989df3b21cceb6c62e6ca5f38fc57d1f7037b
MartinJ95 <martinjones95@gmx.co.uk>
2024-02-14 17:55:23 +0000

    feat planet and particles
    this commit should introduce planets and particle emmitors to the codebase

--- PlunderDogsInSpace/Game/Sandbox.cpp
+++ PlunderDogsInSpace/Game/Sandbox.cpp
@@ -10,6 +10,7 @@ bool Sandbox::Init()
 {
     team1.Init(&team2);
     team2.Init(&team1);
+    testPlanet.m_transform.SetPosition(glm::vec3(0.f, -10.f, -5.f));
 	return Application::Init();
 }
 
@@ -18,12 +19,15 @@ void Sandbox::Update()
     m_controller.Update();
     team1.Update();
     team2.Update();
-	Application::Update();
+    testPlanet.m_transform.SetRotation(testPlanet.m_transform.GetRotation() * glm::quat(glm::vec3(0.f, glm::radians((float)((int)(testPlanet.orbitSpeed * ServiceLocator::GetTimeService().deltaTime) % 360)), 0.f)));
+    testPlanet.m_transform.CheckModelXForm();
+    Application::Update();
 }
 
 void Sandbox::Render()
 {
     m_graphics.GetShader(0).SetRender3D(m_graphics.GetCamera());
+    m_graphics.Render(0, 0, true, testPlanet.m_transform.GetModelXform());
     /*m_graphics.Render(0, 0, true, glm::mat4(
         10.f, 0.f, 0.f, 0.f,
         0.f, 10.f, 0.f, 0.f,
@@ -41,5 +45,6 @@ void Sandbox::EndOfFrame()
     m_controller.EndOfFrame();
     team1.EndOfFrame();
     team2.EndOfFrame();
+    testPlanet.m_transform.EndFrame();
 	Application::EndOfFrame();
 }
