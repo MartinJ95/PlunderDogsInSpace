1713ef3bf231739ea69cb5a2c170879be59fbb3c
MartinJ95 <martinjones95@gmx.co.uk>
2024-03-04 15:17:35 +0000

    fix grid : compile fix
    this commit should allow the grid to now compile on external projects

--- PlunderDogsInSpace/Math/Grid.h
+++ PlunderDogsInSpace/Math/Grid.h
@@ -12,6 +12,8 @@ struct BaseTile
 
 struct TileIndex
 {
+	TileIndex() : first(0), second(0) {}
+	TileIndex(int First, int Second) : first(First), second(Second) {}
 	int first, second;
 };
 
@@ -19,11 +21,49 @@ template<typename T>
 struct HorizontalGridSmall
 {
 public:
-	HorizontalGridSmall(float MinBounds, float MaxBounds);
-	bool IsOnGrid(const glm::vec3& Point);
-	TileIndex GetTilePosition(const glm::vec3& Point);
-	T& GetCurrentTile(const glm::vec3& Point);
-	glm::vec3 GetPointOnTile(const glm::vec3& Point);
+	HorizontalGridSmall(float MinBounds, float MaxBounds) : minBounds(MinBounds), maxBounds(MaxBounds)
+	{
+		minBounds.y = maxBounds.y = 0;
+	}
+	bool IsOnGrid(const glm::vec3& Point) const
+	{
+		if (Point.x < minBounds.x || Point.x > maxBounds.x || Point.z < minBounds.z || Point.z > maxBounds.z)
+			return false;
+		return true;
+	}
+	TileIndex GetTilePosition(const glm::vec3& Point) const
+	{
+		TileIndex indices;
+		indices.first = ((maxBounds.z - Point.z) / (maxBounds.z - minBounds.z)) * SmallGridTileAmount;
+		indices.second = ((maxBounds.x - Point.x) / (maxBounds.x - minBounds.x)) * SmallGridTileAmount;
+		return TileIndex();
+	}
+	T& GetCurrentTile(const glm::vec3& Point)
+	{
+		TileIndex i = GetTilePosition(Point);
+		return tiles
+			[i.first]
+		[i.second];
+	}
+	T& GetTile(const TileIndex& Index)
+	{
+		return tiles[Index.first][Index.second];
+	}
+	glm::vec3 GetPointOnTile(const glm::vec3& Point) const
+	{
+		TileIndex i = GetTilePosition(Point);
+		return glm::vec3(
+			minBounds.x + (((maxBounds.x - minBounds.x) / SmallGridTileAmount) * i.first),
+			0,
+			minBounds.z + (((maxBounds.z - minBounds.z) / SmallGridTileAmount) * i.second));
+	}
+	glm::vec3 GetPointOnTile(const TileIndex& Index) const
+	{
+		return glm::vec3(
+			minBounds.x + (((maxBounds.x - minBounds.x) / SmallGridTileAmount) * Index.first),
+			0,
+			minBounds.z + (((maxBounds.z - minBounds.z) / SmallGridTileAmount) * Index.second));
+	}
 private:
 	glm::vec3 minBounds, maxBounds;
 	T tiles[SmallGridTileAmount][SmallGridTileAmount];
